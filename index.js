59c46cb8f711e8111a39a69aed6fe1e7ef9e202a
Anne Muriuki <muriukianne05@gmail.com>
2024-09-25 14:51:32 +0300

    add arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1=2
+const num2=31
+const multiply =2*31
 
-// Write your code here
+const random=10
+Math.floor(Math.random())> 0
+
+const num3=14
+const num4=5
+const mod=14%5
+
+const max=20
+Math.max(5,10,15,20);
